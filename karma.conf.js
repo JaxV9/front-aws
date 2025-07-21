ccbdc533fb7068f7349b4c18e6fd19d440976d02
JaxV9 <layanj.pro@gmail.com>
2025-07-21 13:57:50 +0200

    up

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,4 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+process.env.CHROME_BIN = require("puppeteer").executablePath();
 
 module.exports = function (config) {
   config.set({
@@ -13,15 +12,10 @@ module.exports = function (config) {
       require("@angular-devkit/build-angular/plugins/karma"),
     ],
     client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
+      jasmine: {},
     },
     jasmineHtmlReporter: {
-      suppressAll: true, // removes the duplicated traces
+      suppressAll: true,
     },
     coverageReporter: {
       dir: require("path").join(__dirname, "./coverage/aws-front"),
@@ -30,19 +24,13 @@ module.exports = function (config) {
     },
     reporters: ["progress", "kjhtml"],
     customLaunchers: {
-      ArcHeadless: {
+      ChromeHeadlessCI: {
         base: "ChromeHeadless",
-        flags: ["--no-sandbox"],
-        executablePath: "/Applications/Arc.app/Contents/MacOS/Arc",
+        flags: ["--no-sandbox", "--disable-gpu", "--disable-dev-shm-usage"],
       },
     },
-    browsers: ["ArcHeadless"],
+    browsers: ["ChromeHeadlessCI"],
     restartOnFileChange: true,
-    files: [
-      "node_modules/angular/angular.js",
-      "node_modules/angular-mocks/angular-mocks.js",
-      "app/**.ts",
-      "tests/**.ts",
-    ],
+    files: [{ pattern: "src/**/*.spec.ts", watched: false }],
   });
 };
